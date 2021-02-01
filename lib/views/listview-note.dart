88e38953bd8a84a0003c80f79982608e43fcdca1
venkateswarlu940 <77621609+venkateswarlu940@users.noreply.github.com>
2021-02-01 17:20:35 +0530

    Add files via upload

---
+++ lib/views/listview-note.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:storage_app/db_helper/note_helper.dart';
+import 'package:storage_app/model/note.dart';
+
+import 'note_screen.dart';
+class ListViewNote extends StatefulWidget {
+  @override
+  _ListViewNoteState createState() => new _ListViewNoteState();
+}
+
+class _ListViewNoteState extends State<ListViewNote> {
+  List<Note> items = new List();
+  DatabaseHelper db = new DatabaseHelper();
+
+  @override
+  void initState() {
+    super.initState();
+
+    db.getAllNotes().then((notes) {
+      setState(() {
+        notes.forEach((note) {
+          items.add(Note.fromMap(note));
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JSA ListView Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView Demo'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: ListView.builder(
+              itemCount: items.length,
+              padding: const EdgeInsets.all(15.0),
+              itemBuilder: (context, position) {
+                return Column(
+                  children: <Widget>[
+                    Divider(height: 5.0),
+                    ListTile(
+                      title: Text(
+                        '${items[position].title}',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.deepOrangeAccent,
+                        ),
+                      ),
+                      subtitle: Text(
+                        '${items[position].description}',
+                        style: new TextStyle(
+                          fontSize: 18.0,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      leading: Column(
+                        children: <Widget>[
+                          Padding(padding: EdgeInsets.all(10.0)),
+                          CircleAvatar(
+                            backgroundColor: Colors.blueAccent,
+                            radius: 15.0,
+                            child: Text(
+                              '${items[position].id}',
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                      trailing:     IconButton(
+                        icon: const Icon(Icons.delete),
+                         onPressed: () =>
+                        _deleteNote(
+                           context, items[position], position)),
+                      //onTap: () => _navigateToNote(context, items[position]),
+                    ),
+                  ],
+                );
+              }),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _createNewNote(context),
+        ),
+      ),
+    );
+  }
+
+  void _deleteNote(BuildContext context, Note note, int position) async {
+    db.deleteNote(note.id).then((notes) {
+      setState(() {
+        items.removeAt(position);
+      });
+    });
+  }
+
+  // void _navigateToNote(BuildContext context, Note note) async {
+  //   String result = await Navigator.push(
+  //     context,
+  //     MaterialPageRoute(builder: (context) => NoteScreen(note)),
+  //   );
+  //
+  //   if (result == 'update') {
+  //     db.getAllNotes().then((notes) {
+  //       setState(() {
+  //         items.clear();
+  //         notes.forEach((note) {
+  //           items.add(Note.fromMap(note));
+  //         });
+  //       });
+  //     });
+  //   }
+  // }
+
+  void _createNewNote(BuildContext context) async {
+    String result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NoteScreen(Note('', ''))),
+    );
+
+    if (result == 'save') {
+      db.getAllNotes().then((notes) {
+        setState(() {
+          items.clear();
+          notes.forEach((note) {
+            items.add(Note.fromMap(note));
+          });
+        });
+      });
+    }
+  }
+}
